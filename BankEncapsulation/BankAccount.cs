0e2dfcc483d109f4c0337cfe26d5c5d4de8456a9
koikiri <kahvisabatino@gmail.com>
2022-10-29 13:06:17 -1000

    Submit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double dollars) 
+        {
+            _balance = dollars + _balance;
+        }
+
+        public double GetBalance() 
+        {
+            return _balance;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myBank = new BankAccount();
+
+            Console.WriteLine("Enter the Amount you would like to Deposit: ");
+            string valueEntered = "";
+            double deposit = 0.0;
+            bool success = false;
+
+            while (success == false) 
+            {
+                valueEntered = Console.ReadLine();
+                success = double.TryParse(valueEntered, out deposit);
+                if (success == false) 
+                {
+                    Console.WriteLine("Enter the Amount you would like to Deposit: ");
+
+                }
+            }
+            
+            myBank.Deposit(deposit);
+            Console.WriteLine("Your new Balance is: ");
+            Console.WriteLine(myBank.GetBalance().ToString("C"));
+
         }
     }
 }
